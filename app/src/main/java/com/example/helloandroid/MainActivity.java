88a82e4797cc7d8385db146ea923b9e718e322b8
rrtran <99690238+rrtran@users.noreply.github.com>
2022-04-04 09:03:20 -0700

    Added comments
    - Comments for MainActivity.java
    - Comments for TipCalculator.java
    - Comments for activity_main.xml
    - Comments for colors.xml

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -21,15 +21,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        tipCalc = new TipCalculator(0.17f, 100.0f);
+        tipCalc = new TipCalculator(0.17f, 100.0f); // Initialize a new tip calculator
         setContentView(R.layout.activity_main);
 
-        billEditText = (EditText)findViewById(R.id.amount_bill);
-        tipEditText = (EditText)findViewById(R.id.amount_tip_percent);
+        billEditText = (EditText)findViewById(R.id.amount_bill); // Get a reference to the amount bill edit box
+        tipEditText = (EditText)findViewById(R.id.amount_tip_percent); // Get a reference to the amount_tip_percent edit box
 
         TextChangeHandler tch = new TextChangeHandler();
-        billEditText.addTextChangedListener(tch);
-        tipEditText.addTextChangedListener(tch);
+        billEditText.addTextChangedListener(tch); // Executes the overridden functions of the TextChangeHandler when the amount_bill edit box has characters inputted in it or deleted from it
+        tipEditText.addTextChangedListener(tch); // Executes the overridden functions of the TextChangeHandler when the amount_tip_percent edit box has its character deleted or when characters are inputted
 
     }
 
@@ -47,49 +47,54 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void afterTextChanged(Editable editable) {
-            calculate();
+            calculate(); // After a text has been inputted or deleted, call this calculate function
         }
     }
 
-    public void calculate(View v) {
-        EditText billEditText = (EditText) findViewById(R.id.amount_bill);
-        EditText tipEditText = (EditText) findViewById(R.id.amount_tip_percent);
-        String billString = billEditText.getText().toString();
-        String tipString = tipEditText.getText().toString();
-        TextView tipTextView = (TextView)findViewById(R.id.amount_tip);
-        TextView totalTextView = (TextView)findViewById(R.id.amount_total);
+    // calculate - computes the total amount by calculating the tip in dollars and adding it to the bill
+    public void calculate() {
+        EditText billEditText = (EditText) findViewById(R.id.amount_bill); // Get a reference to the amount bill edit box
+        EditText tipEditText = (EditText) findViewById(R.id.amount_tip_percent); // Get a reference to the amount_tip_percent edit box
+        String billString = billEditText.getText().toString(); // Get the bill and convert the value to a string
+        String tipString = tipEditText.getText().toString();  // Get the tip percent and convert the value to a string
+        TextView tipTextView = (TextView)findViewById(R.id.amount_tip); // Get a reference to the amount_tip text view
+        TextView totalTextView = (TextView)findViewById(R.id.amount_total); // Get a reference to the amount_total text view
         try {
-            float billAmount = Float.parseFloat(billString);
-            int tipPercent = Integer.parseInt(tipString);
+            float billAmount = Float.parseFloat(billString); // Parse the bill as a float
+            int tipPercent = Integer.parseInt(tipString); // Parse the tip percent as an int
             //update model
-            tipCalc.setBill(billAmount);
-            tipCalc.setTip(0.01f * tipPercent);
-            float tip = tipCalc.tipAmount();
-            float total = tipCalc.totalAmount();
-            tipTextView.setText(money.format(tip));
-            totalTextView.setText(money.format(total));
+            tipCalc.setBill(billAmount); // Set the tip calculator's bill member variable to billAmount
+            tipCalc.setTip(0.01f * tipPercent); // Set the tipCalculator's tip percent
+            float tip = tipCalc.tipAmount();    // Compute the tip and assign it to the tip float variable
+            float total = tipCalc.totalAmount(); // Compute the total amount and assign it to the total float variable
+            tipTextView.setText(money.format(tip)); // Display the tip in the amount_tip text view
+            totalTextView.setText(money.format(total)); // Display the total amount in the amount_total text view
         } catch (NumberFormatException nfe) {
 
         }
     }
 
-    public void calculate() {
-        EditText billEditText = (EditText) findViewById(R.id.amount_bill);
-        EditText tipEditText = (EditText) findViewById(R.id.amount_tip_percent);
-        String billString = billEditText.getText().toString();
-        String tipString = tipEditText.getText().toString();
-        TextView tipTextView = (TextView)findViewById(R.id.amount_tip);
-        TextView totalTextView = (TextView)findViewById(R.id.amount_total);
+    // calculate - computes the total amount by calculating the tip in dollars and adding it to the bill
+    // @param View - More research is needed to determine what the purpose of this parameter is, but
+    //               it is required in order to be referenced in the activity_main.xml file's button's
+    //               onClick attribute...
+    public void calculate(View v) {
+        EditText billEditText = (EditText) findViewById(R.id.amount_bill); // Get a reference to the amount bill edit box
+        EditText tipEditText = (EditText) findViewById(R.id.amount_tip_percent); // Get a reference to the amount_tip_percent edit box
+        String billString = billEditText.getText().toString(); // Get the bill and convert the value to a string
+        String tipString = tipEditText.getText().toString();  // Get the tip percent and convert the value to a string
+        TextView tipTextView = (TextView)findViewById(R.id.amount_tip); // Get a reference to the amount_tip text view
+        TextView totalTextView = (TextView)findViewById(R.id.amount_total); // Get a reference to the amount_total text view
         try {
-            float billAmount = Float.parseFloat(billString);
-            int tipPercent = Integer.parseInt(tipString);
+            float billAmount = Float.parseFloat(billString); // Parse the bill as a float
+            int tipPercent = Integer.parseInt(tipString); // Parse the tip percent as an int
             //update model
-            tipCalc.setBill(billAmount);
-            tipCalc.setTip(0.01f * tipPercent);
-            float tip = tipCalc.tipAmount();
-            float total = tipCalc.totalAmount();
-            tipTextView.setText(money.format(tip));
-            totalTextView.setText(money.format(total));
+            tipCalc.setBill(billAmount); // Set the tip calculator's bill member variable to billAmount
+            tipCalc.setTip(0.01f * tipPercent); // Set the tipCalculator's tip percent
+            float tip = tipCalc.tipAmount();    // Compute the tip and assign it to the tip float variable
+            float total = tipCalc.totalAmount(); // Compute the total amount and assign it to the total float variable
+            tipTextView.setText(money.format(tip)); // Display the tip in the amount_tip text view
+            totalTextView.setText(money.format(total)); // Display the total amount in the amount_total text view
         } catch (NumberFormatException nfe) {
 
         }
--- app/src/main/java/com/example/helloandroid/TipCalculator.java
+++ app/src/main/java/com/example/helloandroid/TipCalculator.java
@@ -4,33 +4,46 @@ public class TipCalculator {
     private float bill;
     private float tip;
 
+    // TipCalculator - Initializes the members variables, bill and tip
     public TipCalculator(float newTip, float newBill) {
         setBill(newBill);
         setTip(newTip);
     }
 
+    // getTip - returns the tip
+    // @return float - the tip
     public float getTip() {
         return tip;
     }
 
+    // getBill - returns the bill
+    // @return float - the bill
     public float getBill() {
         return bill;
     }
 
+    // setTip - assigns a new tip to the tip member variable
+    // @param float - the new tip
     public void setTip(float newTip) {
         if (newTip > 0)
             tip = newTip;
     }
 
+    // setBill - assigns a new bill to the bill member variable
+    // @param float - the new bill
     public void setBill(float newBill) {
         if (newBill > 0)
             bill = newBill;
     }
 
+    // tipAmount - computes the tip by multiplying the bill and tip percent
+    // @return float - the amount to tip
     public float tipAmount() {
         return bill * tip;
     }
 
+    // totalAmount - returns the bill plus tip amount
+    // @return float - the bill plus tip amount
     public float totalAmount() {
         return bill + tipAmount();
     }
