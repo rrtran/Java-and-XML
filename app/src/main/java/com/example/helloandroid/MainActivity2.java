ed78a6001f75d33f670923a6746e3d2572e3cfd4
rrtran <99690238+rrtran@users.noreply.github.com>
2022-04-07 13:32:08 -0700

    Completed TicTacToe version 1
    - Added listener for button clicks
    - Implemented button clicks event handler to draw an 'X' on buttons that are clicked

--- app/src/main/java/com/example/helloandroid/MainActivity2.java
+++ app/src/main/java/com/example/helloandroid/MainActivity2.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -33,14 +35,34 @@ public class MainActivity2 extends AppCompatActivity {
         gridLayout.setRowCount(TicTacToe.SIDE);
 
         buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
+        ButtonHandler buttonHandler = new ButtonHandler();
         for (int row = 0; row < TicTacToe.SIDE; row++) {
             for (int column = 0; column < TicTacToe.SIDE; column++) {
                 buttons[row][column] = new Button(this);
+                buttons[row][column].setTextSize((int)(w * .2));
+                buttons[row][column].setOnClickListener(buttonHandler);
                 gridLayout.addView(buttons[row][column], w, w);
             }
         }
 
         // Set gridLayout as the View of this Activity
         setContentView(gridLayout);
+    }
+
+    public void update(int row, int column) {
+        Log.w("MainActivity", "Inside update: " + row + ", " + column);
+        buttons[row][column].setText("X");
+    }
+    private class ButtonHandler implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Log.w("MainActivity", "Inside onClick, v = " + view);
+            for (int row = 0; row < TicTacToe.SIDE; row++) {
+                for (int column = 0; column < TicTacToe.SIDE; column++) {
+                    if (view == buttons[row][column])
+                        update(row, column);
+                }
+            }
+        }
     }
 }
