cd92af6b0b36bfd2db4ce41fbe4116817007da09
rrtran <99690238+rrtran@users.noreply.github.com>
2022-04-13 14:19:53 -0700

    Completed Mortgage version 2
    - Updated MainActivity3 with mortgage static object
    - Implemented updateView() function in MainActivity3.java
    - implemented updateView() function in DataActivity.java
    - Implemented in updateMortgageObject() function

--- app/src/main/java/com/example/helloandroid/DataActivity.java
+++ app/src/main/java/com/example/helloandroid/DataActivity.java
@@ -1,14 +1,65 @@
 package com.example.helloandroid;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class DataActivity extends androidx.appcompat.app.AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3_data);
+        updateView();
     }
 
     public void goBack(android.view.View v) {
+        updateMortgageObject();
         this.finish();
+    }
+
+    public void updateView() {
+        Mortgage mortgage = MainActivity3.mortgage;
+        if (mortgage.getYears() == 10) {
+            RadioButton radioButton10 = (RadioButton)findViewById(R.id.ten);
+            radioButton10.setChecked(true);
+        }
+        else if (mortgage.getYears() == 15) {
+            RadioButton radioButton15 = (RadioButton)findViewById(R.id.fifteen);
+            radioButton15.setChecked(true);
+        }
+        else {
+            // Do nothing because 30 is already checked by default
+        }
+
+        EditText amountEditText = (EditText)findViewById(R.id.data_amount);
+        amountEditText.setText("" + mortgage.getAmount());
+        EditText rateEditText = (EditText)findViewById(R.id.data_rate);
+        rateEditText.setText("" + mortgage.getRate());
+    }
+
+    public void updateMortgageObject() {
+        Mortgage mortgage = MainActivity3.mortgage;
+        RadioButton radioButton10 = (RadioButton)findViewById(R.id.ten);
+        RadioButton radioButton15 = (RadioButton)findViewById(R.id.fifteen);
+        int years = 30;
+        if (radioButton10.isChecked()) {
+            years = 10;
+        }
+        else if (radioButton15.isChecked()) {
+            years = 15;
+        }
+        mortgage.setYears(years);
+        EditText amountEditText = (EditText)findViewById(R.id.data_amount);
+        String amountString = amountEditText.getText().toString();
+        EditText rateEditText = (EditText)findViewById(R.id.data_rate);
+        String rateString = rateEditText.getText().toString();
+        try {
+            float amount = Float.parseFloat(amountString);
+            mortgage.setAmount(amount);
+            float rate = Float.parseFloat(rateString);
+            mortgage.setRate(rate);
+        } catch (NumberFormatException numberFormatException) {
+            mortgage.setAmount(100000.0f);
+            mortgage.setRate(0.035f);
+        }
     }
 }
--- app/src/main/java/com/example/helloandroid/MainActivity3.java
+++ app/src/main/java/com/example/helloandroid/MainActivity3.java
@@ -3,15 +3,35 @@ package com.example.helloandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
+    public static Mortgage mortgage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mortgage = new Mortgage();
         setContentView(R.layout.activity_main3);
     }
 
+    public void onStart() {
+        super.onStart();
+        updateView();
+    }
+
+    public void updateView() {
+        TextView amountTextView = (TextView) findViewById(R.id.amount);
+        amountTextView.setText(mortgage.getFormattedAmount());
+        TextView yearsTextView = (TextView) findViewById(R.id.years);
+        yearsTextView.setText("" + mortgage.getYears());
+        TextView rateTextView = (TextView) findViewById(R.id.rate);
+        rateTextView.setText(100 * mortgage.getRate() + "%");
+        TextView monthlyPaymentTextView = (TextView)findViewById(R.id.payment);
+        monthlyPaymentTextView.setText(mortgage.formattedMonthlyPayment());
+        TextView totalPaymentTextView = (TextView)findViewById(R.id.total);
+        totalPaymentTextView.setText(mortgage.formattedTotalPayment());
+    }
     public void modifyData(android.view.View v) {
         android.content.Intent myIntent = new android.content.Intent(this, DataActivity.class);
         this.startActivity(myIntent);
